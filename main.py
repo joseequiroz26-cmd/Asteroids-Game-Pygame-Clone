087f4dcd3b0b228b33149bd5660e91fa0c96ae9c
joseeqiroz26-cmd <joseequiroz26@gmail.com>
2026-05-23 20:59:08 -0600

    feat: add Player class and logging
    - Created Player class inheriting from CircleShape
    - Added triangle method to render spaceship shape
    - Overrode draw() to display white triangle
    - Defined PLAYER_RADIUS and LINE_WIDTH in constants.py
    - Integrated logger to generate game_state.jsonl
    - Limited framerate to 60 FPS in main loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT 
 from logger import log_state
+from player import Player
 
 
 
@@ -12,15 +13,25 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill ("black")
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
