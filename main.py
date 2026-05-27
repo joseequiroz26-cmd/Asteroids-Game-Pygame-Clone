34921a457e2e744e937e3a8441df8f7105725156
joseeqiroz26-cmd <joseequiroz26@gmail.com>
2026-05-26 19:47:01 -0600

    We added the colition method, what makes if we collide with an asteroid we lose the game

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT 
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+
 
 
 
@@ -43,6 +45,12 @@ def main():
 
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if player.collides_with (asteroid):
+                log_event ("player_hit")
+                print ("Game over!")
+                sys.exit()
 
         screen.fill("black")
         for item in drawable:
